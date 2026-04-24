f4982e7e42097e707b864ebed75c6aa70c5d9e94
Victor <victor.mosqueracaion@gmail.com>
2026-04-24 19:16:11 +0200

    Update PlanetaEnano.java

--- PlanetaEnano.java
+++ PlanetaEnano.java
@@ -2,19 +2,31 @@ package Hijos;
 import Padre.Planeta;
 public class PlanetaEnano extends Planeta {
 
-	public PlanetaEnano(int vida, int misiles) {
-		super(vida, misiles);
+	public PlanetaEnano() {
+		super(100,50);
 	}
 	public String combate(int misilesLanzados, Planeta atacado) {
+		int misilesImpactados = 0;
 		String evento = "";
-		
-	// Aplicamos el daño final al pobrecito atacado
-			atacado.setVidas(getVidas() - misilesLanzados);
-			this.misilesRonda -= misilesLanzados; // Gastamos nuestros misiles
 
-			evento = "🚀 [" + nombre.replace('.', ' ') + "] lanzó " + misilesLanzados + " misiles a [" + atacado.getNombre().replace('.', ' ') + "]. ";
-
-			evento += "(- " + misilesLanzados + " HP).\n";
-			return evento;
+		// Simulación de esquiva (el Enano tiene un 50% de probabilidad de esquivar cada misil)
+		if (atacado instanceof PlanetaEnano) {
+			for (int i = 0; i < misilesLanzados; i++) {
+				if (Math.random() >= 0.5) {
+					misilesImpactados++; 
+				}
+			}
+		} else {
+			misilesImpactados = misilesLanzados;
 		}
+		// Aplicamos el daño final al planeta atacado
+		atacado.setVidas(atacado.getVidas() - misilesImpactados);
+		this.misilesRonda -= misilesLanzados; // Gastamos nuestros misiles
+		System.out.println("ATENCION CHIVATO:::--->>"+"A "+atacado.getNombre()+" le quedan "+atacado.getVidas()+"hp" );
+
+		evento = "🚀 [" + nombre.replace('.', ' ') + "] lanzó " + misilesLanzados + " misiles a [" + atacado.getNombre().replace('.', ' ') + "], impactaron "+misilesImpactados+".";
+
+		evento += "(- " + misilesImpactados + " HP).\n";
+		return evento;
+	}
 }
