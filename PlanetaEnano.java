347b34a4379d009c7cd3aad9e4c53d8c747a4a60
Victor <victor.mosqueracaion@gmail.com>
2026-04-27 09:54:14 +0200

    Update PlanetaEnano.java

--- PlanetaEnano.java
+++ PlanetaEnano.java
@@ -22,7 +22,6 @@ public class PlanetaEnano extends Planeta {
 		// Aplicamos el daño final al planeta atacado
 		atacado.setVidas(atacado.getVidas() - misilesImpactados);
 		this.misilesRonda -= misilesLanzados; // Gastamos nuestros misiles
-		System.out.println("ATENCION CHIVATO:::--->>"+"A "+atacado.getNombre()+" le quedan "+atacado.getVidas()+"hp" );
 
 		evento = "🚀 [" + nombre.replace('.', ' ') + "] lanzó " + misilesLanzados + " misiles a [" + atacado.getNombre().replace('.', ' ') + "], impactaron "+misilesImpactados+".";
 
